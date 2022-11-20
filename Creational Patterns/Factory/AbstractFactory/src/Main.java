dd627121940fd8cdccf5e5c17dfec9f1f4b4a762
Azzasp <p.erickbatista@gmail.com>
2022-11-20 14:32:57 -0300

    Testing in main

--- Creational Patterns/Factory/AbstractFactory/src/Main.java
+++ Creational Patterns/Factory/AbstractFactory/src/Main.java
@@ -1,7 +1,29 @@
+import factory.FabricaPerifericos;
+import factory.PerifericosErgonomicos;
+import factory.PerifericosGamer;
+import mouse.Mouse;
+import teclado.Teclado;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Criando as fábricas
+        FabricaPerifericos perifericosGamer = new PerifericosGamer();
+        FabricaPerifericos perifericosErgonomicos = new PerifericosErgonomicos();
+
+        //Criando nossos periféricos gamer
+        Mouse mouseGamer = perifericosGamer.criarMouse();
+        Teclado tecladoGamer = perifericosGamer.criarTeclado();
+
+        //Criando nossos periféricos ergonomicos
+        Mouse mouseErgonomico = perifericosErgonomicos.criarMouse();
+        Teclado tecladoErgonomico = perifericosErgonomicos.criarTeclado();
+
+        //Testando respectivos periféricos
+        mouseGamer.clicar();
+        mouseErgonomico.clicar();
+
+        tecladoGamer.teclar();
+        tecladoErgonomico.teclar();
     }
 }
