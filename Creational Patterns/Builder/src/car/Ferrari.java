41819d1808c896684fc0a17b98fd7934ff49f71b
Azzasp <p.erickbatista@gmail.com>
2022-11-21 19:00:24 -0300

    car classes

---
+++ Creational Patterns/Builder/src/car/Ferrari.java
@@ -0,0 +1,12 @@
+package car;
+
+public class Ferrari {
+
+    private String engine;
+    private int seats;
+    private int window;
+    private boolean multimidia;
+    private boolean GPS;
+
+    private Ferrari(){}
+}
---
+++ Creational Patterns/Builder/src/car/Tucson.java
@@ -0,0 +1,11 @@
+package car;
+
+public class Tucson {
+    private String engine;
+    private int seats;
+    private int window;
+    private boolean multimidia;
+    private boolean GPS;
+
+    private Tucson(){}
+}
