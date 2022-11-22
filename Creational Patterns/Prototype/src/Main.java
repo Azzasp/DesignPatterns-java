da6b534812b402db7f374245127d3d4c4962df4c
Azzasp <p.erickbatista@gmail.com>
2022-11-22 20:22:56 -0300

    testing cloning in main

--- Creational Patterns/Prototype/src/Main.java
+++ Creational Patterns/Prototype/src/Main.java
@@ -1,9 +1,10 @@
 import eletronicos.Smartphone;
 import eletronicos.Tablet;
+import prototype.PrototypeRegistros;
 
 public class Main {
     public static void main(String[] args) {
-
+        PrototypeRegistros prototypeRegistros = new PrototypeRegistros();
         //Objetos a serem clonados
         Smartphone smartphone = new Smartphone();
         Tablet tablet = new Tablet();
@@ -17,6 +18,10 @@ public class Main {
 
 
         //Clonagem
+        Smartphone cloneSmartphone = new Smartphone(smartphone);
+        Tablet cloneTablet = new Tablet(tablet);
 
+        //Iterando lista
+        prototypeRegistros.verClones();
     }
 }
