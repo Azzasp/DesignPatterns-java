7b17ff10f12dfeefeb7f532c53a6688a4670927e
Azzasp <p.erickbatista@gmail.com>
2022-11-22 20:18:05 -0300

    main method without clonning system

--- Creational Patterns/Prototype/src/Main.java
+++ Creational Patterns/Prototype/src/Main.java
@@ -1,5 +1,22 @@
+import eletronicos.Smartphone;
+import eletronicos.Tablet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Objetos a serem clonados
+        Smartphone smartphone = new Smartphone();
+        Tablet tablet = new Tablet();
+
+        //adicionando valores aos atributos do objetos
+        smartphone.setTela("Oled");
+        smartphone.setTamanhoEmCm(14);
+
+        tablet.setTela("LCD");
+        tablet.setTamanhoEmCm(30);
+
+
+        //Clonagem
+        
     }
 }
