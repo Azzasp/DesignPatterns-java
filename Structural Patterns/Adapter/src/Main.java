a7e60d5fa3fe62071a5592bfdeeaab70493765f5
Azzasp <p.erickbatista@gmail.com>
2022-11-23 21:30:41 -0300

    testing in main

--- Structural Patterns/Adapter/src/Main.java
+++ Structural Patterns/Adapter/src/Main.java
@@ -1,7 +1,45 @@
+import models.Pessoa;
+import termometro.AdapterFahrenheit;
+import termometro.TermometroDigital;
+import termometro.TermometroMercurio;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Pessoa> listaPessoa = Arrays.asList((
+                new Pessoa("Pedro Erick", 178, 36.0)),
+                new Pessoa("Roberto", 183, 38.0),
+                new Pessoa("Carlos", 175, 38.5),
+                new Pessoa("Martha", 173, 36.5)
+        );
+
+
+
+        System.out.println("Medindo com Termometro Digital \n");
+
+        for(Pessoa p : listaPessoa) System.out.println(
+                p.getNome() + " | medindo: "+
+                new AdapterFahrenheit()
+                .medirTemperaturaCelsius(p)+
+                " | estado fisico: " +
+                p.getEstadoFisico()
+
+        );
+
+        System.out.println("Medindo com Termometro Mercurio \n");
+
+        for(Pessoa p : listaPessoa) System.out.println(
+                p.getNome() + " | medindo: "+
+                new TermometroMercurio()
+                .medirTemperaturaCelsius(p)+
+                " | estado fisico: "
+                + p.getEstadoFisico()
+        );
+
+
     }
 }
